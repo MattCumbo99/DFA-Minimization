b6f242b78b9e52a6114f17f1315c638e746c570f
Matthew Cumbo <mattcumbo99@gmail.com>
2021-03-26 18:09:57 -0500

    Included basic functions

--- Workspace/src/minimizeDFA.java
+++ Workspace/src/minimizeDFA.java
@@ -1,9 +1,18 @@
+import java.io.File;
 
 public class minimizeDFA {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Has to accept one command line argument
+		if (args.length < 1) {
+			System.out.println("Usage: java minimizeDFA filename.dfa");
+			return;
+		}
+		
+		File source = new File(args[0]);
+		DFA theDFA = new DFA(source);
+		
+		theDFA.output();
 	}
 
 }
